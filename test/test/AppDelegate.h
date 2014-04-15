776b265a6a356061f99b4a01f4a22cb06aa2ae41
McKeeLin <mckeelin@icloud.com>
2014-04-15 14:07:55 +0800

    test

---
+++ test/test/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by 林景隆 on 4/15/14.
+//  Copyright (c) 2014 MckeeStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ test/test/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  test
+//
+//  Created by 林景隆 on 4/15/14.
+//  Copyright (c) 2014 MckeeStudio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
